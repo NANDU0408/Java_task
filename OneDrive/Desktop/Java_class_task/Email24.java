dfdd0a645da73d8c08da849ae03233e1b4c3e925
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-07 22:41:19 +0530

    Email24_Commit

---
+++ OneDrive/Desktop/Java_class_task/Email24.java
@@ -0,0 +1,23 @@
+class Email24{
+	String domain;
+	int id;
+	String fName;
+	String lastName;
+	
+	Email24(String domain, int id, String fName, String lastName){
+		this.domain=domain;
+		this.id=id;
+		this.fName=fName;
+		this.lastName=lastName;
+	}
+	
+	
+	void displayInfo(){
+		System.out.println("Starting info in Email");
+		System.out.println("Email domain:"+this.domain);
+		System.out.println("Email id:"+this.id);
+		System.out.println("Email fName:"+this.fName);
+		System.out.println("Email lastName:"+this.lastName);
+		System.out.println("Ending info in Email");
+	}
+}
