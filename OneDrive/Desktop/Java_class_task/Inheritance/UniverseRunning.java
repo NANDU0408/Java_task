65669b98ddb040cd3c06ec997a58c2cad63e9eec
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-16 21:26:28 +0530

    UniverseRunning Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/UniverseRunning.java
@@ -0,0 +1,9 @@
+class UniverseRunning{
+	public static void main(String[] nnn){
+		
+		Universe universe = new Core();
+		Universe un = new SolarSystem();
+		Universe uv = new SpiralGalaxy();
+		Universe ua = new Aestoriod();
+	}
+}
