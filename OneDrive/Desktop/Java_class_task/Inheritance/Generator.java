d9d1b9f963397e5009ecc703d57909b87dac3fc2
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-17 23:19:27 +0530

    Generator Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Generator.java
@@ -0,0 +1,10 @@
+class Generator extends Machine{
+	
+	Generator(){
+		System.out.println("Generator is a machine");
+	}
+	
+	void LoadShedding(){
+		System.out.println("Generator is used during LoadShedding");
+	}
+}
