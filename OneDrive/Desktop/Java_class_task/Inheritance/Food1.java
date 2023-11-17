ee851411a91553187676c4f43c13fc5d270a0bfe
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-17 23:20:26 +0530

    Food1 Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Food1.java
@@ -0,0 +1,10 @@
+class Food1 extends Plant{
+	
+	Food1(){
+		System.out.println("Food is the energy giving element to humans");
+	}
+	
+	void Preparing(){
+		System.out.println("Food is prepared very tasty");
+	}
+}
