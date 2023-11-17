cf5f0b5da686d2494eb28dffc7c1ff367a2c848e
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-17 23:16:38 +0530

    Wood Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Wood.java
@@ -0,0 +1,11 @@
+class Wood{
+	
+	Wood(){
+		System.out.println("");
+		System.out.println("Entering into the world of wood");
+	}
+	
+	void Cutting(){
+		System.out.println("Cutting the wood");
+	}
+}
