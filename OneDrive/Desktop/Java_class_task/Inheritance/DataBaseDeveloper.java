75f6e1fec168c4adc8e39070b72dd0134975419a
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-20 20:21:37 +0530

    DataBaseDeveloper Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/DataBaseDeveloper.java
@@ -0,0 +1,10 @@
+class DataBaseDeveloper extends Developer{
+	
+	DataBaseDeveloper(){
+		System.out.println("DataBaseDeveloper");
+	}
+	
+	void runScripts(){
+		System.out.println("DataBaseDeveloper runs scripts");
+	}
+}
