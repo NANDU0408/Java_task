22881c1398c2197187994a8d03a4b86a2e56f1db
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-20 20:20:22 +0530

    BuisnessAnalyst Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/BuisnessAnalyst.java
@@ -0,0 +1,10 @@
+class BuisnessAnalyst extends Person{
+	
+	BuisnessAnalyst(){
+		System.out.println("BuisnessAnalyst");
+	}
+	
+	void checkRequirement(){
+		System.out.println("Person who is buisnessAnalyst check requirement");
+	}
+}
