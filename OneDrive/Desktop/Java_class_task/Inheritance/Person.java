fe277ac0c049b3c8c7c1158bd93c33ef5f3eb079
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-20 20:18:41 +0530

    Person Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Person.java
@@ -0,0 +1,13 @@
+class Person{
+	Person(){
+		System.out.println("Person");
+	}
+	
+	void eat(){
+		System.out.println("Person eats food to get strength");
+	}
+	
+	void sleep(){
+		System.out.println("Person sleeps to get refresh");
+	}
+}
