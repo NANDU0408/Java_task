a5db68497df403aa731d804e51730e09f4e8efd1
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-20 20:19:38 +0530

    Tester Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Tester.java
@@ -0,0 +1,10 @@
+class Tester extends Developer{
+	
+	Tester(){
+		System.out.println("Tester");
+	}
+	
+	void qualityCheck(){
+		System.out.println("Tester checks the quality of program or software");
+	}
+}
