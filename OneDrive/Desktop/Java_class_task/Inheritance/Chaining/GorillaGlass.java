b58fadc7e653d2d9a7bf8fdf7571d1b516e5a7ae
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-21 22:30:07 +0530

    GorillaGlass Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Chaining/GorillaGlass.java
@@ -0,0 +1,6 @@
+class GorillaGlass extends LaminatedGlass{
+	
+	GorillaGlass(double cost, double warranty, double length, double thickness, GlassType type, String usage, String brand){
+		super(cost,warranty,length,thickness,type,usage,brand);
+}
+}
