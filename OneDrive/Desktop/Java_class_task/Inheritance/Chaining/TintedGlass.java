42a0ae1dfb0fc34eec4e772fd3f40b82dc962ca6
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-21 22:29:33 +0530

    TintedGlass Commit

---
+++ OneDrive/Desktop/Java_class_task/Inheritance/Chaining/TintedGlass.java
@@ -0,0 +1,7 @@
+class TintedGlass extends ToughenedGlass{
+	
+	
+	TintedGlass(double weight, String ownerName, double length, double thickness, GlassType type, String usage, String brand){
+		super(weight,ownerName,length,thickness,type,usage,brand);
+	}
+}
