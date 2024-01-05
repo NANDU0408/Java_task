1183125c3ab3587b8ea006e31b5a4543343659ad
NANDISH0408 <93071634+NANDU0408@users.noreply.github.com>
2024-01-05 21:56:52 +0530

    Add files via upload

---
+++ OneDrive/Desktop/Java_class_task/Intelliji/Collections/Boot/ChariotRunning.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacea.nandishA.collectionJ.Boot;
+
+import com.xworkz.interfacea.nandishA.collectionJ.Chariot;
+
+public class ChariotRunning {
+    public static void main(String[] args) {
+        Chariot chariot = new Chariot();
+        chariot.getChariots();
+    }
+}
